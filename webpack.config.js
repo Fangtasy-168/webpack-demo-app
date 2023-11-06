5af23c039124b9dd3ad46cf60c7243e24cd35a1f
Tony Fang <tonyfang1897@gmail.com>
2023-11-05 21:10:08 -0500

    Files change to SCSS with its necessary Loaders
    - In this case we wanted to change bootstrap styling so we installed
      bootstrap locally with npm install --save-dev bootstrap
    - changed the main.css file to main.scss
    	- deleted previous contented and imported in bootstrap scss
    	  styling
    - npm install --save-dev sass-loader sass webpack
    - changed the test regex to files ending in scss
    - added sass-loader as the first in order under modules -> rules
    - within the main.scss file can now change default styles of bootstrap
    	- $primary: teal
    	- $danger: purple

--- webpack.config.js
+++ webpack.config.js
@@ -10,8 +10,12 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.css/i,
-                use: ['style-loader', 'css-loader'], // note that the array executes in a reverse order before sytle can run the css must be loaded
+                test: /\.scss/i,
+                use: [
+                    'style-loader', // 3.  inputs the commonJs into the DOM under a style tag. 
+                    'css-loader',  // 2. changes css file to a CommonJs 
+                    'sass-loader'  // 1. changes sass to a css 
+                ], // note that the array executes in a reverse order before sytle can run the css must be loaded
             }
         ]
     },
