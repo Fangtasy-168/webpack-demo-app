e47e286e95315461ee3d8351f97eb246e77b10de
Tony Fang <tonyfang1897@gmail.com>
2023-11-05 20:36:10 -0500

    First Loader CSS and Style Loader
    - created a .css file containing a simple body selector background set to purple
    - linked the css file in the index.js, entry point for Webpack, through
      importing "./main.css"
    - npm install --save-dev style-loader css-loader
    - edited the config file to have the modules object with a rules
      array
    - within the rules array a key named test with a regex statment for any files
      ending in css. second key named  use to contain an array of recently
      installed loaders starting in reverse order to load.

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,13 @@ module.exports = {
     output: {
         filename: "main.js",
         path: path.resolve(__dirname, "dist")
-    }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css/i,
+                use: ['style-loader', 'css-loader'], // note that the array executes in a reverse order before sytle can run the css must be loaded
+            }
+        ]
+    },
 }
